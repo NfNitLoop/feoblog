5ba1baf2b0345c32dc89be0152fbba494e9cffec
Cody Casterline <cody.casterline@gmail.com>
2020-09-01 19:42:59 -0700

    Signatures!

--- web-client/index.ts
+++ web-client/index.ts
@@ -5,9 +5,15 @@ import moment from "moment"
 
 import { Item, Post } from "./protos/feoblog"
 
+import * as nacl from "tweetnacl-ts"
+
+
+
 // TODO:
 const reader = new commonmark.Parser()
 const writer = new commonmark.HtmlRenderer({ safe: true})
+const MAX_ITEM_SIZE = 32 * 1024 // 32k
+
 
 // Strictly parse one of these non-ambiguous timestamps. (MUST include time zone.)
 const DATE_FORMATS = [
@@ -41,7 +47,10 @@ var app = new Vue({
         // <3 Moment in that it'll keep the time and offset together:
         timestampMoment: moment(),
 
-        // Used to generate the protobuf-encoded 
+        userID: "",
+        privateKey: "",
+        signature: "",
+        debug: false,
     },
 
     mounted: function() {
@@ -120,24 +129,84 @@ var app = new Vue({
             return item;
         },
 
+        itemProtoBytes: function() {
+            return this.itemProto.serialize()
+        },
+
         itemJson: function() {
             return JSON.stringify(this.itemProto.toObject(), null, 1)
         },
 
-        debug: function() {
-            // TODO: Better way to enable debug:
-            return true
-            return this.post.startsWith("!!!debug")
-        },
-
         protoSize: function() { 
-            return (this.itemProto as Item).serialize().length
+            return this.itemProtoBytes.length
         },
 
         protoHex: function() {
-            return bufferToHex(this.itemProto.serialize())
+            return bufferToHex(this.itemProtoBytes)
         },
+
+        // This post is valid and signed and ready to send to the server:
+        valid: function(): boolean {
+            return this.errors.length == 0
+        },
+
+        errors: function() {
+            return [... this.calculateErrors()]
+        },
+
+        validSignature: function(): boolean {
+            if (!this.userID || !this.signature) {
+                return false
+            }
+            try {
+                let pubKey = bs58.decode(this.userID)
+                let signature = bs58.decode(this.signature)
+                let ok = nacl.sign_detached_verify(this.itemProtoBytes, signature, pubKey)
+                return ok;
+            } catch (error) {
+                console.error("Error validating signature:", error)
+                return false
+            }
+        }
         
+    },
+
+    watch: {
+        privateKey: function(newValue) {
+            console.debug(`Got pkey of length ${newValue.length}`)
+            let buf;
+            try {
+                buf = bs58.decode(newValue)
+            } catch (error) {
+                console.error("Error during base58 decode", error)
+                return
+            }
+
+            if (buf.length != 32) {
+                console.debug("Want 32 bytes, but found", buf.length)
+                return
+            }
+            
+            // TODO: Consider using base58Check so that we can verify valid
+            // pkeys if someone tries to type one in.
+
+            let keypair = nacl.sign_keyPair_fromSeed(buf);
+            this.userID = bs58.encode(keypair.publicKey)
+            // TODO: sign the message.
+            let signature = nacl.sign_detached(this.itemProtoBytes, keypair.secretKey)
+            this.signature = bs58.encode(signature)
+
+            // Delete the privateKey, we don't want to save it any longer than
+            // necessary:
+            this.privateKey=""
+           
+        },
+
+        post: function(newValue) {
+            if (newValue.startsWith("!!!debug")) {
+                this.debug = true;
+            }
+        }
     },
 
     methods: {
@@ -146,9 +215,30 @@ var app = new Vue({
             box.focus();
             box.selectionStart = 0;
             box.selectionEnd = box.value.length;
+        },
+
+        calculateErrors: function*() {
+            if (!this.userID) {
+                yield "Must sign the message"
+            }
+
+            let itemLength = this.itemProtoBytes.length
+            if (itemLength > MAX_ITEM_SIZE) {
+                yield `Item size is ${itemLength}/${MAX_ITEM_SIZE}`
+            }
+
+            if (this.timeInputError) {
+                yield this.timeInputError
+            }
+
+            // TODO: Check signature.
+            if (!this.validSignature) {
+                yield "Invalid Signature"
+            }
         }
     }
 });
+
 
 function bufferToHex (x) {
     return [...new Uint8Array (x)]
