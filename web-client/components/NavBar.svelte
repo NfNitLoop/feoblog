b85fb96cdd1438df5f352973b243312b762fbe24
Cody Casterline <cody.casterline@gmail.com>
2021-01-06 10:50:43 -0800

    Checkpoint: Client SPA work
    I want to change the client into a single-page(ish) application, and
    I'm using tino, but:
    https://github.com/AlexxNB/tinro/issues/42
    
    Updating Svelte itself didn't fix things, next I'll try a global update.

---
+++ web-client/components/NavBar.svelte
@@ -0,0 +1,10 @@
+<div class="navBar">
+<input type="text" name="currentUserID" value="{currentUserID}" placeholder="Current user ID"/>
+</div>
+
+<script lang="ts">
+
+// TODO: Load from storage. Provide a pretty UI.
+$: currentUserID = ""
+
+</script>
