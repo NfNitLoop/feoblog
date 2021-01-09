293bcd4dac08bf5abf5fc7b52c96de8baaae377f
Cody Casterline <cody.casterline@gmail.com>
2021-01-09 15:19:23 -0800

    WIP working on a popup

--- web-client/components/IndexPage.svelte
+++ web-client/components/IndexPage.svelte
@@ -3,23 +3,32 @@
     <div class="nav-container">
         <div class="nav">
             <a href="#/">Home</a>
+            <button on:click={popup.toggle}>{buttonText}</button>
         </div>
     </div>
 
     <div class="items">
         <Router {routes}/>
     </div>
+
 </div>
+
+
+<Popup title="Settings" bind:this={popup} bind:shown={popupShown}>
+    Just testing
+    <div>This is a div</div>
+    <br><input type="text" name="foobar" placeholder="Some text">
+</Popup>
 
 <script context="module" lang="ts">
     import Router from "svelte-spa-router"
 
-    import NavBar from "./NavBar.svelte"
     import ItemView from "./ItemView.svelte"
     import IndexPageDefault from "./IndexPageDefault.svelte"
     import NotFoundPage from "./NotFoundPage.svelte"
     import PostPage from "../post/post.svelte"
     import CreateIDPage from "../create_id/create.svelte"
+    import Popup from "./Popup.svelte"
     
     const routes = {
         "/": IndexPageDefault,
@@ -28,9 +37,16 @@
         "/create_id/": CreateIDPage,
         "*": NotFoundPage,
     }
+</script>
+
+<script lang="ts">
+
+let popup
+let popupShown
+$: buttonText = popupShown ? "Hide" : "Show"
+$: {
+    console.log("popupShown", popupShown)
+}
 
 </script>
 
-<style>
-
-</style>
--- web-client/components/NotFoundPage.svelte
+++ web-client/components/NotFoundPage.svelte
@@ -1,7 +1,3 @@
 <div>
     <p>Page not NotFound</p>
 </div>
-
-<script>
-    export let params = {}
-</script>
