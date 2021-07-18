8d8fddb8246d215dd1916629bc38f941f48998d3
Cody Casterline <cody.casterline@gmail.com>
2021-07-18 16:08:22 -0700

    Fix nav link highlight bug.
    After changing users, the current page was not properly
    displayed when cliking different navigation links.
    
    I don't think there *is* a way to update using use:active,
    so now we just use our own component for nav links.

---
+++ web-client/components/NavLink.svelte
@@ -0,0 +1,24 @@
+<!--
+    svelte-spa-router/active can't update when the nav links are changed.
+    See: https://github.com/ItalyPaleAle/svelte-spa-router/issues/187 😢
+    This component works around that.
+-->
+<a class:active {href}><slot></slot></a>
+
+<script lang="ts">
+import { location } from "svelte-spa-router"
+
+export let href: string
+
+// TODO: We'll likely need to make this more permissive if I ever decide
+// to try allowing ?params on hash URLs like:
+// #/post?replyTo=wharrgarbl.
+$: active = href === `#` + $location
+
+</script>
+
+<style>
+.active {
+    color: black;
+}
+</style>
