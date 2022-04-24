255d2f78427c498a9fa91205f6c3d4df28008c39
Cody Casterline <cody.casterline@gmail.com>
2022-04-23 17:15:34 -0700

    identicon: don't block async task.
    ... however, I don't think this was the bottleneck.
    Rust debug builds are just surprisingly slow. :p

--- src/server/non_standard.rs
+++ src/server/non_standard.rs
@@ -3,7 +3,8 @@
 //! These are not part of the documented FeoBlog standard, but are used by this
 //! particular FeoBlog implementation to provide extra features.
 
-use actix_web::{HttpResponse, web::Path};
+use actix_web::{HttpResponse, web::Path, Responder};
+use futures::Future;
 use identicon::IdenticonJSOptions;
 
 use crate::backend::UserID;
@@ -11,7 +12,18 @@ use crate::backend::UserID;
 /// This is not really defined as part of the standard for FeoBlogs.
 /// BUT, having a default user image is handy when implementing the Open Graph Protocol.
 /// (... which is itself also not a strict requirement for a FeoBlog.)
-pub(crate) fn identicon_get(Path(user_id): Path<UserID>) -> HttpResponse {
+pub(crate) async fn identicon_get(Path(user_id): Path<UserID>) -> Result<HttpResponse,HttpResponse> {
+    let icon = actix_web::web::block(move || identicon_get_sync(user_id))
+        .await
+        .map_err(|e| HttpResponse::InternalServerError().finish())?;
+
+    Ok(HttpResponse::Ok()
+        .content_type("image/png")
+        .body(icon)
+    )
+}
+
+fn identicon_get_sync(user_id: UserID) -> Result<Vec<u8>, ()> {
     use identicon::{Identicon, Mode::IdenticonJS};
 
     // Note: Must be >=16 bytes, but userIDs are bigger:
@@ -20,15 +32,12 @@ pub(crate) fn identicon_get(Path(user_id): Path<UserID>) -> HttpResponse {
         .background_rgb(255, 255, 255)
     ;
 
-    let mut png = vec![];    
-    if let Err(err) = icon.to_png(&mut png) {
-        return HttpResponse::InternalServerError()
-            .body(format!("Error: {}", err));
-    }
+    let mut png = vec![];   
+    icon.to_png(&mut png)
+        // Can't actually reference the error type. Boo.
+        .map_err(|e| ())?;
 
-    HttpResponse::Ok()
-        .content_type("image/png")
-        .body(png)
+    Ok(png)
 }
 
 /// server-absolute identicon URL. ex:  /u/__/icon.png
