900c28520e5263fc2f0ab5cd4c3032e64bdf0d2f
Cody Casterline <cody.casterline@gmail.com>
2024-01-02 08:20:58 -0800

    Initial port of some styles.
    Sass is so much nicer than CSS, kicking myself for
    not having used it earlier.

--- crates/leptos_app/src/lib.rs
+++ crates/leptos_app/src/lib.rs
@@ -13,7 +13,8 @@ pub fn App() -> impl IntoView {
         <Title text="FeoBlog Leptos Placeholder"/>
         <Router>
             <Routes>
-                <Route path="/leptos" view=Greeter/>
+                <Route path="/leptos/" view=Greeter/>
+                <Route path="/leptos/home" view=Home/>
             </Routes>
         </Router>
     }
@@ -25,7 +26,7 @@ pub fn Greeter() -> impl IntoView {
     let get_name = move || name.get();
     let node = create_node_ref::<Input>();
 
-    let evt = create_effect(move |_| {
+    let _evt = create_effect(move |_| {
         node.get().map(|n| {
             n.select()
         });
@@ -40,5 +41,50 @@ pub fn Greeter() -> impl IntoView {
             }
             node_ref=node
         />
+        <div>
+            <A href="/leptos/home">Home</A>
+        </div>
+    }
+}
+
+#[component]
+pub fn Home() -> impl IntoView {
+    view! {
+        <article>
+            <article-head>
+                <img
+                    class="profileImage"
+                    src="/u/C1KxN5L4tYjEv6vkxjD8mMD917ntKTG5r1vKYmhJgyQX/icon.png"
+                    alt="identicon"/>
+
+                    <a class="userID" 
+                        href="#/u/C1KxN5L4tYjEv6vkxjD8mMD917ntKTG5r1vKYmhJgyQX/" 
+                        title="C1KxN5L4tYjEv6vkxjD8mMD917ntKTG5r1vKYmhJgyQX"
+                    >"Mastodon Feed"</a>
+                    
+                    <a class="timestamp" 
+                        href="#/u/C1KxN5L4tYjEv6vkxjD8mMD917ntKTG5r1vKYmhJgyQX/i/4AxdtMxv5U9kbXDYT3PyneEH4Xn7k8Wr3DGYaAFF5FVjDVR894E2S7K1WXpkSaeqwyfyvPomjeKi5aRq764zvmpx/" 
+                        title="2024-01-01 23:19:10 +0000\n2024-01-01 15:19:10 -0800">"3 hours ago"</a>
+
+                <Arrow/>
+            </article-head>
+            <article-body>
+                <p>"Lorum ipsum dolor sit amet ..."</p>
+            </article-body>
+        </article>
+
+        <round-box>
+            <h1>Heading</h1>
+            <p>"Here's a sample paragraph."</p>
+        </round-box>
+    }
+}
+
+#[component]
+pub fn Arrow() -> impl IntoView {
+    view! {
+        <svg class="openArrow left" viewBox="0 0 200 200" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M67 34L133 100L67 166" stroke="black" stroke-width="13" stroke-linecap="round" class="svelte-vh3cio"></path>
+        </svg>
     }
 }
